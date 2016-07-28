d26506288c6f290fd710082715901d58fd9a3ce7
Jonas Manuel <jmanuel@msopb.de>
2016-07-28 21:45:01 +0200

    added requests to setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         packages=find_packages('src'),  # dependency resolution
         namespace_packages=['son', ],
         include_package_data=True,
-        install_requires=['flask', 'sqlalchemy'],
+        install_requires=['flask', 'sqlalchemy', 'requests'],
         zip_safe=False,
         entry_points={
             'console_scripts': [
