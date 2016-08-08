0beabfa8df6eaf621071c8857fb4af7a0732d124
Jonas Manuel <jmanuel@msopb.de>
2016-08-08 15:04:10 +0200

    changed main to __main__

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     zip_safe=False,
     entry_points={
         'console_scripts': [
-            'son-editor=son.editor.app:main',
+            'son-editor=son.editor.app:__main__',
         ],
     },
     test_suite='son',
