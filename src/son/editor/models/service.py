5ad24b0d078855ba92baf2af2361c88cca61160b
Christian Korfmacher <c.korfmac@mail.upb.de>
2016-08-15 16:51:37 +0200

    Implemented service + test

--- src/son/editor/models/service.py
+++ src/son/editor/models/service.py
@@ -11,6 +11,7 @@ class Service(Base):
     name = Column(String(50))
     vendor = Column(String(50))
     version = Column(String(50))
+    uid = Column(String(150), unique=True)
     project_id = Column(Integer, ForeignKey('project.id'))
     project = relationship("Project", back_populates="services")
 
@@ -18,12 +19,12 @@ class Service(Base):
         self.name = name
         self.vendor = vendor
         self.version = version
+        self.uid = "%s:%s:%s" % (vendor, name, version)
         if project:
             self.project_id = project.id
 
     def __repr__(self):
         return '<Service %r>' % (self.name)
 
-class ServiceEncoder(JSONEncoder):
-    def default(self,o):
-        return {'id' : o.id, 'name' : o.name}
+    def as_dict(self):
+        return {'id': self.id, 'name': self.name, 'vendor': self.vendor, 'version': self.version, 'uid': self.uid}
--- src/son/editor/tests/serviceapi_test.py
+++ src/son/editor/tests/serviceapi_test.py
@@ -4,6 +4,7 @@ import json
 from son.editor.models.workspace import Workspace
 from son.editor.models.project import Project
 from son.editor.models.user import User
+from son.editor.models.service import Service
 
 from son.editor.app.database import db_session
 
@@ -21,12 +22,15 @@ class ServiceAPITest(unittest.TestCase):
         self.project = Project(name="Project A")
         self.workspace = Workspace(name="Workspace A")
         self.user = User(name="username", email="foo@bar.com")
+        self.service = Service(name="Service a", vendor="de.upb", version="1.0")
 
         # Add some relationships
         self.workspace.owner = self.user;
         self.project.workspace = self.workspace
+        self.service.project = self.project
 
         db_session.add(self.project)
+        db_session.add(self.service)
         db_session.add(self.workspace)
         db_session.add(self.user)
         db_session.commit()
@@ -35,15 +39,45 @@ class ServiceAPITest(unittest.TestCase):
         db_session.delete(self.project)
         db_session.delete(self.workspace)
         db_session.delete(self.user)
+        db_session.delete(self.service)
         db_session.commit()
 
-    def test_create(self):
+    def test_create_service(self):
         postArg = json.dumps({"vendor": "de.upb.cs.cn.pgsandman",
                               "name": "Service Name",
                               "version": "0.0.1"})
-
         response = self.app.post("/" + constants.WORKSPACES + "/" + str(self.workspace.id)
                                  + "/" + constants.PROJECTS + "/" + str(self.project.id)
                                  + "/" + constants.SERVICES + "/", headers={'Content-Type': 'application/json'},
                                  data=postArg)
         self.assertTrue(response.status_code == 201)
+
+    def test_get_services(self):
+        response = self.app.get("/" + constants.WORKSPACES + "/" + str(self.workspace.id)
+                                + "/" + constants.PROJECTS + "/" + str(self.project.id)
+                                + "/" + constants.SERVICES + "/")
+
+        service = json.loads(response.data.decode())
+        self.assertEqual(service[0]['vendor'], "de.upb")
+
+        self.assertTrue(response.status_code == 200)
+
+    def test_update_service(self):
+        postArg = json.dumps({"vendor": "de.upb.cs",
+                              "name": "Service Name",
+                              "version": "1.0"})
+
+        response = self.app.put("/" + constants.WORKSPACES + "/" + str(self.workspace.id)
+                                + "/" + constants.PROJECTS + "/" + str(self.project.id)
+                                + "/" + constants.SERVICES + "/" + str(self.service.id),
+                                headers={'Content-Type': 'application/json'},
+                                data=postArg)
+        service = json.loads(response.data.decode())
+        self.assertEqual(service['vendor'], "de.upb.cs")
+
+    def test_delete_service(self):
+        response = self.app.delete("/" + constants.WORKSPACES + "/" + str(self.workspace.id)
+                                   + "/" + constants.PROJECTS + "/" + str(self.project.id)
+                                   + "/" + constants.SERVICES + "/" + str(self.service.id),
+                                   headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 200)
