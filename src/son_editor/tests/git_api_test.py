6f1953dad8c472b243ebfeea4cc9527c8be3832c
Christian Korfmacher <c.korfmac@mail.upb.de>
2017-01-16 15:12:29 +0100

    Fixed keyerror by accessing environment variables

--- src/son_editor/tests/git_api_test.py
+++ src/son_editor/tests/git_api_test.py
@@ -7,8 +7,8 @@ from son_editor.util.context import init_test_context
 
 # Get access token
 
-github_user = os.environ["github_bot_user"] if not CONFIG['github_bot_user'] else CONFIG['github_bot_user']
-github_access_token = os.environ["github_access_token"] if not CONFIG['github_access_token'] else CONFIG[
+github_user = os.environ["github_bot_user"] if not 'github_bot_user' in CONFIG else CONFIG['github_bot_user']
+github_access_token = os.environ["github_access_token"] if not 'github_access_token' in CONFIG else CONFIG[
     'github_access_token']
 
 
@@ -52,7 +52,7 @@ class GitAPITest(unittest.TestCase):
         response = self.app.post("/" + constants.WORKSPACES + "/" + self.wsid + "/" + constants.GIT + "/init",
                                  headers={'Content-Type': 'application/json'},
                                  data=json.dumps(arg))
-        json_data = json.loads(response.data.decodeO())
+        json_data = json.loads(response.data.decode())
         self.assertTrue(json_data['success'], "true")
 
         # 2. Create and push git repository
